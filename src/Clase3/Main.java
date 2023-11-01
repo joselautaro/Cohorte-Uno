222ef47ba6ca9e714d9da122ab4b9111bf7d8dd9
Lautaro Montaña <joselautarom@gmail.com>
2023-11-01 19:01:03 -0300

    Clases 2, 3, 4

---
+++ src/Clase3/Main.java
@@ -0,0 +1,11 @@
+
+package Clase3;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println(" Clase3! ");
+    }
+    
+}
---
+++ src/Clase4/Clase4.java
@@ -0,0 +1,113 @@
+package Clase4;
+
+
+
+import javax.swing.JOptionPane;
+
+
+public class Clase4 {
+
+    public static void main(String[] args) {
+        
+        /*
+        String oracion = "Cohorte uno se egresa en: ";
+        
+        int edad = 2024;
+        
+        String resultado = oracion + edad;
+        
+        JOptionPane.showMessageDialog(null, resultado);
+        */
+        /*
+        int edad = 30;
+        
+        int edadProxima = 31;
+        
+        int resultado = edad + edadProxima;
+        
+        JOptionPane.showMessageDialog(null, resultado);
+        */
+        
+        int numeroUno = 10;
+        
+        int numeroDos = 0;
+        
+        int resultado = numeroUno / numeroDos;
+        
+        System.out.println(resultado);
+        
+        
+        
+        /*
+        let numero = "texto";       
+        */
+        
+        
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Palabra package: nos indica que esta java class proviene
+del package clase4
+ */
+
+ /*
+    public:  es una palabra que determina un modificador de acceso
+    en este caso, esta java puede ser acceso desde cualquier otro punto de nuestra app
+    
+    class: es una palabra reservada que determina la estructura básica de una clase
+ */
+
+ /*
+        public: es un modificador este nos permite tener accesible esta java desde cualquier punto de la app   
+        
+        static: es una  palabra reservada que nos indica esta clase es estática
+        
+        void: es una palabra reservada que nos indica que este método, NO DEVUELVE NADA
+        
+        main: es una palabra reservada que nos indica que en esta clase se ejecuta un método principal
+ */
+//System.out.println(" Clase 4 ");
+/*
+        Tipos de datos primitivos en java: Son 8
+   
+   Numeros enteros
+   
+   int o  Integer: 4 bytes de espacio de almacenamiento en memoria y va desde -2.147.483.684 hasta 2.147.483.647
+   
+   short: 2 bytes de espacio de almacenamiento en memoria y van desde -32.768 hasta 32.267
+   
+   long: 8 bytes de espacio de almacenamiento en memoria y el subfijo L 50.000.000.000.000
+   
+   byte: 1 byte de espacio de almacenamiento en memoria y va desde -128 hasta 127
+   
+   _____________________________________________________________________________________
+   
+   Numeros decimales
+   
+   float: 4 bytes de espacio de almacenamiento en memoria y van desde 6 o 7 cifras decimales
+   
+   utiliza un subfijo F 5,787956
+   
+   double: 8 bytes de espacio de almacenamiento en memoria y van desde 15 cifras decimales 5,589647314856975
+   
+   char: representa caracteres de la 'a' a la 'z'
+   
+   boolean: true o false | verdadero o falso
+ */
