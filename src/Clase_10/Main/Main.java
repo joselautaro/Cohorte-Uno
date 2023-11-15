d6362931c11a8e611c6c877da3685eee5e71c6e9
Lautaro Montaña <joselautarom@gmail.com>
2023-11-15 18:57:14 -0300

    Clase 10

---
+++ src/Clase_10/Main/Main.java
@@ -0,0 +1,14 @@
+package Clase_10.Main;
+
+import Clase_10.Persona;
+
+public class Main {
+    
+    public static void main(String[] args) {
+            Persona persona1 = new Persona(31, "Goku", 
+                    "Pérez", "Kame House 45454", 1.80);
+            
+            persona1.mostrarDatos();
+    }
+    
+}
