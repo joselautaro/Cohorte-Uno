5f2ebf94603b0d2771a3d79ee540da933bb716fd
Lautaro Montaña <joselautarom@gmail.com>
2023-11-22 18:08:36 -0300

    Clase 11 y Clase 12

---
+++ src/Clase_11/Principal.java
@@ -0,0 +1,17 @@
+package Clase_11;
+
+
+public class Principal {
+    
+    public static void main(String[] args){
+        
+        //Instanciar Estudiantes
+        Estudiantes estudiante = new Estudiantes("Luciano", "Nuñez", 36, "Ingenieria de Software", 1500, 2024);
+        
+        estudiante.mostrarDatos();
+        
+        
+        
+    }
+    
+}
